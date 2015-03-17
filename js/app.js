a2a73169ef887075908b86b4bc89881ec7537c09
redllamas <sam@jetsets.jp>
2015-03-17 14:47:10 +0900

    second commit

--- js/app.js
+++ js/app.js
@@ -9,13 +9,31 @@
     price: 10.95,
     description: 'better language than javascript??',
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images: [
+      {
+        full: 'image1-full.jpg',
+        thumb: 'image1-thumb.jpg'
+      },
+      {
+        full: 'image2-full.jpg',
+        thumb: 'image2-thumb.jpg'
+      }]
   },
   {
     name: 'Dodecahedron',
     price: 2.95,
     description: 'what a lovely gem',
     canPurchase: true,
-    soldOut: true
+    soldOut: true,
+    images: [
+      {
+        full: 'image1-full.jpg',
+        thumb: 'image1-thumb.jpg'
+      },
+      {
+        full: 'image2-full.jpg',
+        thumb: 'image2-thumb.jpg'
+      }]
   }];
 })();
--- readme.md
+++ readme.md
@@ -2,7 +2,16 @@
 
 Based on Code School's 'Shaping Up with Angular.js'
 
-progress up to level 1.5 Built-in Directives
+##### 2.1 Filters and a New Directive
+
+filters
+  
+>> {{data | filter:options}}  
+>> eg: {{'1388838882732' | date:'MM/dd/yyyy @ h:mma'}}
+>> <li ng-repeat="product in store.products | limitTo:3">
+>> <li ng-repeat="product in store.products | orderBy:'-price'">
+
+##### 1.5 Built-in Directives
 
 DMCE
   - Directives
